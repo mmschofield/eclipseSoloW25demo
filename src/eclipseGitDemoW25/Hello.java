1db22d62cf8f09adb36bc60d8f89eec595d26054
McLaine Schofield <mschofi3@emich.edu>
2025-02-03 12:05:33 -0500

    Did some nifty stuff

--- src/eclipseGitDemoW25/Hello.java
+++ src/eclipseGitDemoW25/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Print statement");
+		System.out.println("Another line");
 	}
 
 }
