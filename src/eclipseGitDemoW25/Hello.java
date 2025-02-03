546c6440b217d58b168ddc900f1578c74618624f
McLaine Schofield <mschofi3@emich.edu>
2025-02-03 12:36:21 -0500

    Programmer 1 added a print statement

--- src/eclipseGitDemoW25/Hello.java
+++ src/eclipseGitDemoW25/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 		System.out.println("Print statement");
 		System.out.println("Another line");
 		System.out.println("Programmer 2 did this ");
+		System.out.println("Programmer 1 did this");
 		}
 
 }
